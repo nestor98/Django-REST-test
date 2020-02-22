9e02ad5b2c7ad3552331752a0aa4fe9c7dd2644b
nestor <nestormg98@gmail.com>
2020-02-22 19:07:27 +0100

    Funcionar funciona, entenderlo ya pa otro dia

--- restest/restapp/admin.py
+++ restest/restapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+
+## Ejemplo:
+# from IS_app.models import Group4Property, ...
+#
+# admin.site.register(Group4Property)
--- restest/restest/settings.py
+++ restest/restest/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +76,13 @@ WSGI_APPLICATION = 'restest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': r'D:\Universidad\Proyecto Software\Django-REST-test\restest\restest\mysql.cnf',
+        },
     }
 }
-
+ #'mysql.cnf',
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
